222ffc4f1211e1cd701dd533029274c635ee32cb
Dimacrypto31 <Dsheplyakov@gmail.com>
2026-03-06 11:23:21 +0300

    test main PASSED, true base_page, main_page , test_main_page

---
+++ pages/login_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+
+    def should_be_login_url(self):
+        # Проверка, что в текущем URL есть подстрока "login"
+        assert "login" in self.browser.current_url, "Login URL is not correct"
+
+    def should_be_login_form(self):
+        # Проверка, что форма логина присутствует
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # Проверка, что форма регистрации присутствует
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
 
 
 def test_guest_should_see_login_link(browser):
@@ -6,3 +8,8 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()        # выполняем метод страницы — переходим на страницу логина
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
