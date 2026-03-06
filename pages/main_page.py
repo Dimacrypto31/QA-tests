323a0f4204411b4addbc01c1e39f9c6433419ea6
Dimacrypto31 <Dsheplyakov@gmail.com>
2026-03-06 11:49:20 +0300

    test should be fing login in testURL

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,9 +8,12 @@ class MainPage(BasePage):
             "Login link is not presented"
         login_link.click()
 
+
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
-            "Login link is not presented"
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_form(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_FORM), \
--- test_main_page.py
+++ test_main_page.py
@@ -7,9 +7,6 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()        # выполняем метод страницы — переходим на страницу логина
-
+    page.go_to_login_page()        # выполняем метод страницы — переходим на страницу логина
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
+    login_page.should_be_login_page()
